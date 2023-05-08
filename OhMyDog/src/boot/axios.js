3ea6d572aee87a361b08d4b946d95b45b51c3310
Joaco <JoaquinDiez3977@gmail.com>
2023-05-08 14:47:27 -0300

    gitignore v3

--- OhMyDog/src/boot/axios.js
+++ OhMyDog/src/boot/axios.js
@@ -1,21 +1,7 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://190.191.175.12:5137" });
-
-api.interceptors.request.use((config) => {
-  /*
-  ESTO SE EJECUTA CADA VEZ Q HAGO UNA PETICION CON AXIOS DESDE EL FRONT
-  OBTIENE EL TOKEN Y LO MANDA EN req.headers.authorization
-  */
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = token;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+const api = axios.create({ baseURL: "http://localhost:5137" });
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
