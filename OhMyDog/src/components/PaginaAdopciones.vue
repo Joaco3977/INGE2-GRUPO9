e16e75a44d96ea79260daf1ad05f39a12c648571
Julia Saenz <54420958+juliasaenz@users.noreply.github.com>
2023-05-08 13:47:11 -0300

    saque un comentario

--- OhMyDog/src/components/PaginaAdopciones.vue
+++ OhMyDog/src/components/PaginaAdopciones.vue
@@ -2,78 +2,8 @@
   <!-- Todo el contenido tiene que estar adentro de un div -->
   <!-- Pueden usar componentes dentro de este componente -->
 
-  <!-- <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey q-pt-lg"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="perrosOtros" label="¡Adoptá un perro!" />
-          <q-tab v-if="rol > 0" name="perrosPropios" label="Mis perros en adopción" />
-    </q-tabs>
-
-    <q-scroll-area
-      :thumb-style="thumbStyle"
-      :bar-style="barStyle"
-      style="height: 70vh"
-      class="bg-white "
-    >
-
-      <q-card flat>
-
-
-        <q-separator />
-
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="perrosOtros">
-            <div class="full-width row items-center">
-              <TarjetaAdopcion
-                v-for="(perro, indice) of perrosDatos" :key="indice"
-                :rol='rol'
-                :servicio='servicioActual'
-                :nombre='perro.nombre'
-                :edad='perro.edad'
-                :tamanio='perro.tamanio'
-                :raza='perro.raza'
-                :comentario='perro.comentario'
-                :contacto='perro.contacto'
-              />
-            </div>
-          </q-tab-panel>
-
-          <q-tab-panel v-if="rol > 0" name="perrosPropios" style="height: full">
-            <div class="full-width column items-center">
-              <q-btn color="accent" class="q-ma-md" style="width: 25em">
-                <q-icon left size="3em" name="ion-heart-empty" />
-                <div class="textoBoton">¡Poné un perro en adopción! </div>
-              </q-btn>
-
-              <div class="full-width row items-center">
-              <TarjetaAdopcion
-                v-for="(perro, indice) of perrosDatos" :key="indice"
-                :rol='rol'
-                :servicio='misAdopciones'
-                :nombre='perro.nombre'
-                :edad='perro.edad'
-                :tamanio='perro.tamanio'
-                :raza='perro.raza'
-                :comentario='perro.comentario'
-                :contacto='perro.contacto'
-              />
-            </div>
-            </div>
-          </q-tab-panel>
-        </q-tab-panels>
-      </q-card>
-
-   
-    </q-scroll-area>
-    -->
   <div class="bg-white full-height full-width" style="height: 97vh">
-    <div class="text-center text-h4 text-primary q-pt-md">ADOPCIONES</div>
+    <div class="titulo text-center text-h4 text-primary q-pt-md">ADOPCIONES</div>
     <q-card stretch flat class="bg-white full-width full-height">
       <q-tabs
         v-model="tab"
@@ -125,7 +55,7 @@
           <q-scroll-area
             :thumb-style="thumbStyle"
             :bar-style="barStyle"
-            style="height: 75vh; width:100%"
+            style="height: 60vh; width:100%"
             class="bg-white full-width"
           >
 
