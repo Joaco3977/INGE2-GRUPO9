69dac061755a74c742a82135b17d85286f5b6801
Julia Saenz <54420958+juliasaenz@users.noreply.github.com>
2023-05-10 17:27:02 -0300

    aguante el pincha

--- OhMyDog/src/components/PaginaConsultaCliente.vue
+++ OhMyDog/src/components/PaginaConsultaCliente.vue
@@ -1,3 +1,11 @@
 <template>
-  <h2>Aca harian las consultas los clientes</h2>
+  
+  <div>
+    <h2>Aca harian las consultas los clientes</h2>
+     <q-img
+      src="https://i.imgflip.com/kg6zg.jpg?a467544"
+      spinner-color="white"
+      style="height: 400px; max-width:300px"
+    />
+  </div>
 </template>
