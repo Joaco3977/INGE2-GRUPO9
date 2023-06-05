371ec5a6febc2d59a46b61cd130cb66e45b09e04
Joaco <JoaquinDiez3977@gmail.com>
2023-06-05 19:21:57 -0300

    Donaciones ya funcional, pero no dona xd

--- OhMyDog/src/components/PaginaDonaciones.vue
+++ OhMyDog/src/components/PaginaDonaciones.vue
@@ -1,7 +1,129 @@
 <template>
-  <h1>Pagina Donaciones</h1>
+  <div class="bg-white" style="width: full; max-height: 99vh">
+    <div
+      class="flex row q-mx-xl justify-between items-center"
+      style="height: 4em"
+    >
+      <div class="titulo text-center text-h4 text-bold text-primary">
+        DONACIONES
+      </div>
+      <q-btn
+        v-if="rol == 2"
+        @click="abrirForm = true"
+        color="accent"
+        class=""
+        style="width: max-content; height: max-content"
+      >
+        <div class="textoBoton" s>Agregar campaña</div>
+      </q-btn>
+    </div>
+
+    <q-separator />
+
+    <q-scroll-area
+      :thumb-style="thumbStyle"
+      :bar-style="barStyle"
+      style="height: 86vh"
+      class="bg-white"
+    >
+      <div
+        v-if="donaciones.length > 0"
+        class="full-width row wrap justify-center"
+      >
+        <TarjetaDonacion
+          @eliminarDonacion="eliminarDonacion"
+          class="q-px-sm col-stretch"
+          v-for="donacion in donaciones"
+          :key="donacion.ID"
+          :rol="rol"
+          :id="donacion.ID"
+          :nombre="donacion.NOMBRE"
+          :descripcion="donacion.DESCRIPCION"
+          :link="donacion.LINK"
+        />
+      </div>
+      <div
+        class="row textoNoItems justify-center full-height content-center q-pa-xl"
+        v-else
+      >
+        ¡Todavía no tenemos ninguna donación en el sitio!
+      </div>
+    </q-scroll-area>
+
+    <q-dialog persistent v-model="abrirForm" class="">
+      <formDonacion @registrarDonacion="registrarDonacion" :nombreDonaciones="nombreDonaciones" />
+    </q-dialog>
+  </div>
 </template>
 
 <script>
+import { defineComponent } from "vue";
+import { ref } from "vue";
+import TarjetaDonacion from "./tarjetas/TarjetaDonacion.vue";
+import { api } from "../boot/axios.js";
+import { useStore } from "../pinia/store.js";
+import formDonacion from "./formularios/formDonacion.vue";
 
+export default defineComponent({
+  name: "PaginaDonaciones",
+  components: {
+    TarjetaDonacion,
+    formDonacion,
+  },
+  setup() {
+    const donaciones = ref([]);
+    const rol = useStore().rol;
+    const abrirForm = ref(false);
+
+    const nombreDonaciones = ref([]);
+
+    const loadDonaciones = async () => {
+      try {
+        const response = await api.get("/donacion/getDonaciones");
+        if (response !== false) {
+          donaciones.value = response.data;
+          nombreDonaciones.value = response.data.map((donacion) => donacion.NOMBRE);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const registrarDonacion = async (donacion) => {
+      try {
+        await api.post("/donacion/addDonacion", {
+          campania: donacion,
+        });
+        abrirForm.value = false;
+        loadDonaciones();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    async function eliminarDonacion(id) {
+      try {
+        await api.post("donacion/deleteDonacion", {
+          id: id,
+        });
+        loadDonaciones();
+      } catch {
+        console.error("NO SE PUDO ELIMINAR CAMPAÑA");
+      }
+    }
+
+    return {
+      eliminarDonacion,
+      donaciones,
+      rol,
+      loadDonaciones,
+      registrarDonacion,
+      abrirForm,
+      nombreDonaciones,
+    };
+  },
+  mounted() {
+    this.loadDonaciones();
+  },
+});
 </script>
---
+++ OhMyDog/src/components/formularios/formDonacion.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="flex">
+    <q-card style="width: 40rem">
+      <q-card-section class="bg-secondary">
+        <div class="text-h5 text-uppercase text-white text-center text-bold" >Agregar campaña</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-md">
+        <div class="text-h6 q-pb-xs text-center text-primary"> Información básica </div>
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+          <q-input
+            class="q-px-lg"
+            filled
+            v-model="nombre"
+            label="Nombre"
+          />
+
+          <q-input
+            class="q-px-lg"
+            filled
+            v-model="descripcion"
+            label="Descripcion"
+          />
+
+          <q-input
+            class="q-px-lg"
+            filled
+            v-model="link"
+            label="Link"
+          />
+
+          <ul class="q-mx-md q-py-xs">
+          <li v-for="mnsj in mensajeError" :key="mnsj" class="bg-white text-accent text-bold">
+            {{ mnsj }}
+          </li>
+          </ul>
+
+          <div class="row justify-end">
+            <q-btn label="Registrar campaña" :disabled="!camposValidos" type="submit" color="accent" />
+            <q-btn
+              label="Cancelar"
+              type="reset"
+              color="primary"
+              flat
+              class="q-ml-sm"
+              v-close-popup
+            />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "formDonacion",
+  props: {
+    nombreDonaciones: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+
+    const nombre = ref("");
+    const descripcion = ref("");
+    const link = ref("");
+
+    const getDatosDonacion = () => {
+      const donacion = {
+        NOMBRE: nombre.value,
+        DESCRIPCION: descripcion.value,
+        LINK: link.value,
+      }
+      return donacion
+    }
+
+    return {
+      nombre,
+      descripcion,
+      link,
+      getDatosDonacion,
+
+      onReset() {
+        nombre.value = null;
+        descripcion.value = null;
+        link.value = null;
+      },
+    };
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        const donacion = this.getDatosDonacion()
+        this.$emit('registrarDonacion', donacion);
+      } catch (error){
+        console.error(error)
+      }
+    }
+  },
+  computed: {
+    mensajeError(){
+      let sError = [];
+      if (!this.nombreValido ){
+        sError.push( "El nombre no es correcto" )
+      }
+      if (this.nombreExiste ){
+        sError.push( "Ese nombre ya esta registrado" )
+      }
+      if (!this.linkValido ){
+        sError.push( "El link no puede estar vacio" )
+      }
+      return sError
+    },
+    nombreValido(){
+      return this.nombre.length > 0 && /^[A-Za-zÀ-ÿ\s]+$/.test(this.nombre);
+    },
+    linkValido(){
+      return this.link.length > 0;
+    },
+    nombreExiste(){
+      return this.nombreDonaciones.includes(this.nombre);
+    },
+    camposValidos(){
+      return this.nombreValido && !this.nombreExiste && this.linkValido;
+    },
+  }
+});
+</script>
