bf27e30a3aa865c6cf1709d037c82f790eb5d64b
Joaco <JoaquinDiez3977@gmail.com>
2023-06-06 18:30:11 -0300

    pago tarjeta prueba

--- OhMyDog/src/components/tarjetas/TarjetaDonacion.vue
+++ OhMyDog/src/components/tarjetas/TarjetaDonacion.vue
@@ -80,7 +80,7 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          Ingrese un monto a donar mayor que 0
+          Ingrese un monto a donar:
         </q-card-section>
 
         <q-input
@@ -95,11 +95,59 @@
             flat
             label="Donar"
             :disabled="!montoValido"
-            @click="realizarDonacion(link)"
+            @click="pagoTarjeta = true"
             color="primary"
             v-close-popup
           />
-          <q-btn flat label="Cancelar"  @click="reset()" color="primary" v-close-popup />
+          <q-btn flat label="Cancelar"  @click="resetMonto()" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="pagoTarjeta">
+      <q-card>
+        <q-card-section>
+          <div class="textoTituloTarjeta text-primary">Pagar con tarjeta</div>
+        </q-card-section>
+
+        <q-input
+          class="q-px-lg"
+          filled
+          v-model="tarjeta.numero"
+          label="Numero de la tarjeta"
+        />
+
+        <q-input
+          class="q-px-lg"
+          filled
+          v-model="tarjeta.titular"
+          label="Titular de la tarjeta"
+        />
+
+        <q-input
+          class="q-px-lg"
+          filled
+          v-model="tarjeta.vencimiento"
+          label="Fecha de vencimiento"
+        />
+
+        <q-input
+          class="q-px-lg"
+          filled
+          v-model="tarjeta.codigo"
+          label="Codigo de seguridad"
+        />
+
+        <q-card-actions align="right">
+          <q-btn
+            flat
+            label="PagarTarjeta"
+            :disabled="!camposValidos"
+            @click="realizarDonacion()"
+            color="primary"
+            v-close-popup
+          />
+          <q-btn flat label="Cancelar"  @click="resetTarjeta()" color="primary" v-close-popup />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -107,6 +155,7 @@
 </template>
 
 <script>
+import { useStore } from "src/pinia/store";
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
@@ -119,36 +168,54 @@ export default defineComponent({
     descripcion: String,
     link: String,
   },
-  setup() {
-    
+  setup(props) {
+
     const donacion = ref({
-      dniCliente: "",
-      nombreDonacion: "",
+      dniCliente: useStore().dni,
+      nombreDonacion: props.nombre,
       monto: "",
     });
+
+    const tarjeta = ref({
+      numero: '',
+      titular: '',
+      vencimiento: '',
+      codigo: ''
+    })
 
     return {
       confirmar: ref(false),
       donar: ref(false),
+      pagoTarjeta: ref(false),
       donacion,
+      tarjeta,
     };
   },
   methods: {
     realizarDonacion(link) {
       //Aca hariamos lo q se tenga q hacer cuadno donamos
-      console.log("Realizar donacion al link: ", link);
+      console.log("Realizar donacion: ", this.donacion);
     },
     eliminarDonacion(id) {
       this.$emit("eliminarDonacion", id);
     },
-    reset(){
+    resetMonto(){
       this.donacion.monto = "";
+    },
+    resetTarjeta(){
+      this.tarjeta.numero = '';
+      this.tarjeta.titular = '';
+      this.tarjeta.vencimiento = '';
+      this.tarjeta.codigo = '';
     }
   },
   mounted() {},
   computed: {
     montoValido() {
-      return /^\d+$/.test(this.donacion.monto) && parseInt(this.donacion.monto) > 0;
+      return /^\d+$/.test(this.donacion.monto) && parseInt(this.donacion.monto) > 0 && parseInt(this.donacion.monto) < 1000000;
+    },
+    camposValidos() {
+      return this.tarjeta.numero.length > 0 && this.tarjeta.titular.length > 0 && this.tarjeta.vencimiento.length > 0 && this.tarjeta.codigo.length > 0;
     },
   },
 });
