079c65fb43f3eff4002e6ed21b6d2372b3b95f53
Renzo Gigena <ren.sabee@gmail.com>
2023-06-21 11:30:08 -0300

    correcion tabala historial

--- DataBase/historial.js
+++ DataBase/historial.js
@@ -6,7 +6,7 @@ const Consola = require ('./serverFunctions.js')
 const Log = require ('./log.js')
 
 router.post('/getHistorial', async (req,res) => {
-    await knex('historial').where('IDPERRO', req.body.id)
+    await knex('historialperro').where('IDPERRO', req.body.id)
     .then((response) => {
         Consola.mensaje("\x1b[35m%s\x1b[0m",`VETERINARIO solicito historial del perro ${req.body.id}`)
         res.status(200).send(response)
@@ -26,7 +26,7 @@ router.post('/agregarEntrada', async (req,res) => {
         COMENTARIO: req.body.entrada.COMENTARIO,
         NOMBREVACUNA: req.body.entrada.NOMBREVACUNA
     }
-    await knex('historial').insert(nuevaEntrada)
+    await knex('historialperro').insert(nuevaEntrada)
     .then(() => {
         res.status(200).send({})
     })
