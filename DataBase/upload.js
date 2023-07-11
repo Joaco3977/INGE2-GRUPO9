fbb2c31108382e2d88e5cb77f649619bb7e5ead5
Joaco <JoaquinDiez3977@gmail.com>
2023-07-11 19:25:33 -0300

    enrutamiento fotos

--- DataBase/upload.js
+++ DataBase/upload.js
@@ -1,10 +1,10 @@
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'uploads')
+        cb(null,`imagenes/${req.body.seccion}`)
     },
     filename: function(req,file,cb){
-        cb(null, `${req.body.seccion}-${req.body.id}.ppg`)
+        cb(null, `${req.body.id}.png`)
     }
 })
 
